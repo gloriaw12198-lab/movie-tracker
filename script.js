0d3d1dbd17ebdeae52ad382a5d74e47e299fd75b
Gloria <gloriaw12198@gmail.com>
2026-02-16 03:59:22 +0300

    added entries

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
 function getItems() {
   const data = localStorage.getItem("items");
   return data ? JSON.parse(data) : [];
-}
+}function saveItems(items) {
+  localStorage.setItem("items", JSON.stringify(items));
+}document.addEventListener("DOMContentLoaded", function () {
+  const form = document.getElementById("itemForm");
+
+
+
